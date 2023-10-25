3e612c26ac61378a516b0188e4bfff53f2b1002a
Rushikesh <rushikeshshetye40@gmail.com>
2023-10-25 12:39:05 +0530

    Task 101 completed

--- src/main/java/in/rushikesh/Application.java
+++ src/main/java/in/rushikesh/Application.java
@@ -10,9 +10,11 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		
-		
+
 		System.out.println("Task 102 completed.");
+
+		System.out.print("Task 101 is ongoing");
+
 	}
 
 }
